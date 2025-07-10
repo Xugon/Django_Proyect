bd008f96a80735790d61049ea800c02e8fa8e8d8
Santiago <santiagoguxon@gmail.com>
2025-07-10 02:03:48 -0300

    Cambios generales

--- aplicacion/urlsapp.py
+++ aplicacion/urlsapp.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.index),
-    path('about/', views.variableabout),
-    path("hola/<str:username>",views.variablehola),
-    path('project/', views.projects),
-    path('tareas/', views.tareas)
+    path("",views.index,name="index"),
+    path('about/', views.variableabout,name="about"),
+    path("hola/<str:username>",views.variablehola,name="hola"),
+    path('project/', views.projects,name="projects"),
+    path('tareas/', views.tareas,name="tareas"),
+    path('create_task/', views.create_task,name="create_task")
 ]
--- aplicacion/views.py
+++ aplicacion/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, JsonResponse
 from .models import Project, Tareas
-from django.shortcuts import get_object_or_404,render
+from django.shortcuts import get_object_or_404,render, redirect
+from .forms import CreateNewTask
 # Create your views here.
 def index (request):
     title = "Bienvenido a Curso de Django !"
@@ -31,3 +32,11 @@ def tareas(request):
     return render(request, "tareas.html",{
         "tareas" : tareas
     })    
+def create_task(request):
+    if request.method == "GET":
+        return render(request, "create_task.html",{
+            "form": CreateNewTask()
+        })
+    else:
+        Tareas.objects.create(title=request.POST["title"], description=request.POST["description"],project_id=2) #type: ignore
+        return redirect("/tareas/")
